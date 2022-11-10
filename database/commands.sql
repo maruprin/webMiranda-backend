b3c7a38c8b37295a0194d64b42d735c2f4caa2b1
Maru Di Primio <mariana.diprimio@gmail.com>
2022-11-10 13:54:38 +0100

    add connection.js

--- database/commands.sql
+++ database/commands.sql
@@ -1,23 +1,31 @@
-CREATE DATABASE mirandadb;
+CREATE DATABASE miranda;
 
-USE mirandadb;
+USE miranda;
+
+CREATE TABLE photos(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  url VARCHAR(255)
+);
+
+CREATE TABLE facilities(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255)
+);
 
 CREATE TABLE rooms (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     number VARCHAR(255),
-    facilities VARCHAR(255),
     bed_type VARCHAR(255),
     status VARCHAR(255),
     rate INT,
     description VARCHAR(255),
-    offer INT,
-    discount INT, 
-    name VARCHAR(255),
-    PRIMARY KEY (id)
-); 
+    offer BOOLEAN,
+    discount INT,
+    name VARCHAR(255)
+);
 
 CREATE TABLE bookings (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     room_id INT,
     guest VARCHAR(255),
     order_date DATE NOT NULL,
@@ -26,32 +34,68 @@ CREATE TABLE bookings (
     special_request varchar(500),
     room_type VARCHAR(255),
     status TINYINT,
-    PRIMARY KEY (id),
     FOREIGN KEY (room_id) REFERENCES rooms(id)
-); 
+);
 
 CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255),
     contact VARCHAR(255),
-    photo VARCHAR(255),
+    photo_id INT,
     work_position VARCHAR(255),
     start_date DATE NOT NULL,
     status BOOLEAN,
     password VARCHAR(255),
     job_desk VARCHAR(255),
-    PRIMARY KEY (id)
-); 
+    FOREIGN KEY (photo_id) REFERENCES photos(id)
+);
 
 CREATE TABLE contact (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     customer VARCHAR(255) NOT NULL,
     email VARCHAR(255),
     date DATE NOT NULL,
     phone VARCHAR(255),
     subject VARCHAR(255),
     comment varchar(500),
-    archive BOOLEAN,
-    PRIMARY KEY (id)
-); 
+    archive BOOLEAN
+);
+
+CREATE TABLE bookings_rooms(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    room_id INT,
+    booking_id INT,
+    FOREIGN KEY (room_id) REFERENCES rooms(id),
+    FOREIGN KEY (booking_id) REFERENCES bookings(id)
+);
+
+CREATE TABLE rooms_photos(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    room_id INT,
+    photo_id INT,
+    FOREIGN KEY (room_id) REFERENCES rooms(id),
+    FOREIGN KEY (photo_id) REFERENCES photos(id)
+);
+
+CREATE TABLE rooms_facilities(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    room_id INT,
+    facility_id INT,
+    FOREIGN KEY (room_id) REFERENCES rooms(id),
+    FOREIGN KEY (facility_id) REFERENCES facilities(id)
+);
+
+//crear un usuario
+CREATE USER 'user_miranda'@'localhost' IDENTIFIED BY 'MIFXT+6mtd4yHPCs';
+
+//dar permisos a un usuario
+GRANT PRIVILEGE ON miranda.* TO 'user_miranda'@'localhost';
+GRANT ALL PRIVILEGES ON miranda . * TO 'user_miranda'@'localhost';
+FLUSH PRIVILEGES;
+
+//para ver usuarios
+SELECT User, Host FROM mysql.user;
+
+//para ver permisos de un usuario
+SHOW GRANTS FOR 'user_miranda'@'localhost';
---
+++ database/connection.js
@@ -0,0 +1,28 @@
+const mysql = require("mysql");
+const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE, MYSQL_SALT } =
+  process.env;
+
+const connection = mysql.createConnection({
+  host: MYSQL_HOST,
+  user: MYSQL_USER,
+  password: MYSQL_PASSWORD,
+  database: MYSQL_DATABASE,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error(err);
+    return console.error("ERROR: " + err.message);
+  }
+  console.log("Connected to the MySQL server");
+});
+
+export function dbQuery(query, params) {
+  return new Promise((resolve, reject) => {
+    connection.query(query, params, (error, results) => {
+      if (error) reject(error);
+      resolve(results);
+    });
+  });
+}
+export default connection;
